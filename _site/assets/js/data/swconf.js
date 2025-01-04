0f6de9e63ee6db889a4a38ac922e97dfc1fbea2c
RudyBlack <rudy1011100@gmail.com>
2025-01-04 22:41:58 +0900

    feat: diary

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735927657',resources: [
+    cacheName: 'chirpy-1735998057',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
